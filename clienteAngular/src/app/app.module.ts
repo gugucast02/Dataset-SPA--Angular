0a2e303e3e6fe30d937997a41002fda5a651cfa3
george yaguana <sebyaguana@gmail.com>
2023-07-18 19:21:51 -0500

    Terminado guia 14

--- clienteAngular/src/app/app.module.ts
+++ clienteAngular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './nav/navigation/navigation.component';
+import { MastheadComponent } from './mast/masthead/masthead.component';
+import { AboutComponent } from './ab/about/about.component';
+import { ServicesComponent } from './ser/services/services.component';
+import { PortfolioComponent } from './port/portfolio/portfolio.component';
+import { CalltoactionComponent } from './call/calltoaction/calltoaction.component';
+import { ContactComponent } from './cont/contact/contact.component';
+import { FooterComponent } from './foot/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MastheadComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    CalltoactionComponent,
+    ContactComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
