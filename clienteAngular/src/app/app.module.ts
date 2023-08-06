34d0a09c95615bf6edf42146569d4f0419558b50
george yaguana <sebyaguana@gmail.com>
2023-08-06 17:31:35 -0500

    correciones

--- clienteAngular/src/app/app.module.ts
+++ clienteAngular/src/app/app.module.ts
@@ -2,22 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './nav/navigation/navigation.component';
 import { AboutComponent } from './ab/contenido/about.component';
 import { ServicesComponent } from './ser/services/services.component';
 import { CalltoactionComponent } from './call/Tablas/calltoaction.component';
 import { HttpClientModule } from  '@angular/common/http';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { NavigationComponent } from './nav/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
     ServicesComponent,
     CalltoactionComponent,
+    NavigationComponent,
     AboutComponent,
-    SidebarComponent,
 
     
   ],
